497f488daebad57c4631e1e48fcc18e90dfb91cd
Administrator <Administrator@MS-PKGPZAASVQNP>
2020-05-12 22:31:48 +0800

    add cors policy

---
+++ AspDotNetCore3/Extensions/CorsPolicyExtension.cs
@@ -0,0 +1,24 @@
+﻿using Core.SqlSugar.Base;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace AspDotNetCore3.Extensions
+{
+    /// <summary>
+    /// CorsPolicyExtension
+    /// </summary>
+    public static class CorsPolicyExtension
+    {
+        public static IServiceCollection AddCorsPolicy(this IServiceCollection serviceCollection, Action<SugarOption> option)
+        {
+            serviceCollection.AddCors(option => option.AddPolicy("cors",
+                policy =>
+                policy.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .AllowAnyOrigin()));
+
+            return serviceCollection;
+        }
+    }
+}
--- AspDotNetCore3/Extensions/SqlsugarExtension.cs
+++ AspDotNetCore3/Extensions/SqlsugarExtension.cs
@@ -1,11 +1,6 @@
 ﻿using Core.SqlSugar.Base;
-using Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
-using SqlSugar;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AspDotNetCore3.Extensions
 {
