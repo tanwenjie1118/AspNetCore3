193f7314ee7a60273a9302c539ab3a779debca2d
Administrator <Administrator@MS-PKGPZAASVQNP>
2020-05-13 22:53:10 +0800

    hangfire not work fix

--- Services/Imp/JobServices.cs
+++ Services/Imp/JobServices.cs
@@ -1,17 +1,22 @@
 ﻿using Core.Redis;
 using Hangfire;
 using Hangfire.Common;
+using Microsoft.Extensions.Logging;
 using System;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace Services.Application
 {
     public class JobServices : IJobServices
     {
         private readonly IRedisRepository redis;
+        private readonly ILogger<JobServices> logger;
         private readonly IRecurringJobManager recurringJob;
-        public JobServices(IRedisRepository redis, IRecurringJobManager recurringJob)
+        public JobServices(IRedisRepository redis, ILogger<JobServices> logger, IRecurringJobManager recurringJob)
         {
             this.redis = redis;
+            this.logger = logger;
             this.recurringJob = recurringJob;
         }
 
@@ -19,15 +24,31 @@ namespace Services.Application
         {
             recurringJob.AddOrUpdate(
                 Guid.NewGuid().ToString(),
-                Job.FromExpression(
-                () =>
-                redis.SetKeyValue(Guid.NewGuid().ToString(), DateTime.Now.ToString(), null)),
-                Cron.Minutely(),
+               new Job(typeof(JobServices).GetMethod("Print")),
+                //Job.FromExpression(() => Console.WriteLine("==================> Now is time  ")),
+                "0/10 * * * * ?",
+                //redis.SetKeyValue(Guid.NewGuid().ToString(), DateTime.Now.ToString(), null)),
+                // Cron.Minutely(),
                 new RecurringJobOptions()
                 {
-                    QueueName = "jservice",
-                    TimeZone = TimeZoneInfo.Utc
+                    //QueueName = "jservice",
+                    //TimeZone = TimeZoneInfo.Utc
                 });
+        }
+
+        //public void Print()
+        //{
+        //    Task.Run(() =>
+        //    {
+        //        var task = "==================> Now is time  " + DateTime.Now;
+        //        Console.WriteLine(task);
+        //    }).Wait();
+        //}
+
+        public void Print()
+        {
+            var task = "==================> Now is time  " + DateTime.Now;
+            Console.WriteLine(task);
         }
     }
 }
