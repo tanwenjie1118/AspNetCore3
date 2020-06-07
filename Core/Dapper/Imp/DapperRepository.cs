9e39c64627a5cab7b2874aa018f2e67d451ef2ef
Administrator <Administrator@MS-PKGPZAASVQNP>
2020-06-07 20:38:48 +0800

    add linq to dapper

--- Core/Dapper/Imp/DapperRepository.cs
+++ Core/Dapper/Imp/DapperRepository.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Linq;
 using Core.Dapper.Base;
-using Dapper;
 using System.Data;
-
+using Kogel.Dapper.Extension.MySql;
 namespace Core.Dapper.Imp
 {
     public class DapperRepository : IDapperRepository
@@ -18,32 +17,42 @@ namespace Core.Dapper.Imp
 
         public int Delete<T>(Expression<Func<T, bool>> func) where T : class, new()
         {
-            throw new NotImplementedException();
+            if (func == null)
+                return connection.CommandSet<T>().Delete();
+            return connection.CommandSet<T>().Delete(Get(func));
         }
 
         public T Get<T>(Expression<Func<T, bool>> func = null)
         {
-            return connection.QueryFirstOrDefault<T>("select * from " + nameof(T));
+            if (func == null)
+                return connection.QuerySet<T>().Get();
+            return connection.QuerySet<T>().Where(func).Get();
         }
 
         public List<T> GetList<T>(Expression<Func<T, bool>> func = null)
         {
-            return connection.Query<T>("select * from " + nameof(T)).ToList();
+            if (func == null)
+                return connection.QuerySet<T>().ToList();
+            return connection.QuerySet<T>().Where(func).ToList();
         }
 
         public List<T> GetPagedList<T>(int pageIndex, int pageSize, out int totalCount, out int tatalPage, Expression<Func<T, bool>> func = null)
         {
-            throw new NotImplementedException();
+            var list = func == null ? connection.QuerySet<T>() : connection.QuerySet<T>().Where(func);
+            var plist = list.PageList(pageIndex, pageSize);
+            tatalPage = plist.TotalPage;
+            totalCount = plist.Total;
+            return plist.Items;
         }
 
         public int Insert<T>(T entity) where T : class, new()
         {
-            throw new NotImplementedException();
+            return connection.CommandSet<T>().Insert(entity);
         }
 
         public int Insert<T>(List<T> entities) where T : class, new()
         {
-            throw new NotImplementedException();
+            return connection.CommandSet<T>().Insert(entities);
         }
     }
 }
--- Tests/Services/DapperTest.cs
+++ Tests/Services/DapperTest.cs
@@ -29,5 +29,46 @@ namespace Tests.Services
 
             Assert.NotNull(coms);
         }
+
+        [Fact]
+        public void GetAll()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            var list = dapper.GetList<Company>();
+            sw.Stop();
+
+            var timespan = sw.ElapsedMilliseconds;
+
+            timespan.ShouldNotBe(0);
+
+            list.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public void InsertModel()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var list = new List<Company>();
+            var range = Enumerable.Range(1, 10000).ToList();
+
+            range.ForEach((x) =>
+            {
+                list.Add(new Company()
+                {
+                    Name = "tan",
+                    No = new Random().Next(0, 9999).ToString(),
+                    Version = x.ToString()
+                });
+            });
+
+            var coms = dapper.Insert(list);
+            sw.Stop();
+
+            var timespan = sw.ElapsedMilliseconds;
+            coms.ShouldBeGreaterThan(0);
+        }
     }
 }
