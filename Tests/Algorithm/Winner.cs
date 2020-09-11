f3ed5ffedd7ed96a950a7860876efcbf0666b88c
hal_t <hal_t@DESKTOP-C46QC8G>
2020-09-11 15:22:54 +0800

    update

--- Tests/Algorithm/Winner.cs
+++ Tests/Algorithm/Winner.cs
@@ -17,16 +17,19 @@ namespace Tests.Algorithm
     public class Winner
     {
         [Fact]
-        public void Test1()
+        public void DynamicProgramming()
         {
 
         }
 
         [Fact]
-        public void Test2()
+        public void Iteration()
         {
-            var flag = CanWin(new int[] { 1, 5, 2 });
+            //var flag = CanWin(new int[] { 1, 5, 2 });
             //var flag = CanWin(new int[] { 2, 4, 55, 6, 8 });
+            //var arr = new int[] { 3606449, 6, 5, 9, 452429, 7, 9580316, 9857582, 8514433, 9, 6, 6614512, 753594, 5474165, 4, 2697293, 8, 7, 1 };
+            var arr = new int[] { 0, 0, 7, 6, 5, 6, 1 };
+            var flag = CanWin(arr);
             flag.ShouldBeTrue();
         }
 
@@ -34,6 +37,8 @@ namespace Tests.Algorithm
         {
             int A = 0;
             int B = 0;
+            if (nums.Length % 2 == 0)
+                return true;
 
             var f = Func(nums.ToList(), A, B);
             return f;
@@ -44,14 +49,15 @@ namespace Tests.Algorithm
             var copy = new int[list.Count];
             list.CopyTo(copy);
             var list1 = copy.ToList();
-            // 从前取第一个数
-            if (AGetFrontSide(list, A, B))
+
+            // 从前取第一个A
+            if (Run(true, list, A, B))
             {
                 return true;
             }
 
-            // 从后取第一个数
-            if (AGetEndSide(list1, A, B))
+            // 从后取第一个A
+            if (Run(false, list1, A, B))
             {
                 return true;
             }
@@ -59,18 +65,26 @@ namespace Tests.Algorithm
             return false;
         }
 
-        private bool AGetFrontSide(List<int> list, int A, int B)
+        private bool Run(bool ishead, List<int> list, int A, int B)
         {
-            // 玩家一需要通过取值 找到一个方法永远可以赢玩家二
-            var first = list.FirstOrDefault();
-            A += first;
-            StackPop(list, true);
+            if (ishead)
+            {
+                var first = list.FirstOrDefault();
+                A += first;
+                StackPop(list, true);
+            }
+            else
+            {
+                var last = list.LastOrDefault();
+                A += last;
+                StackPop(list, false);
+            }
 
             if (list.Count == 0)
             {
                 if (A >= B)
                 {
-                    Debug.WriteLine($"当前 A =={A} B == {B}");
+                    //Debug.WriteLine($"当前 A =={A} B == {B}");
                     return true;
                 }
                 else
@@ -84,7 +98,7 @@ namespace Tests.Algorithm
                 StackPop(list, false);
                 if (A >= B)
                 {
-                    Debug.WriteLine($"当前 A =={A} B == {B}");
+                    //Debug.WriteLine($"当前 A =={A} B == {B}");
                     return true;
                 }
                 else
@@ -115,71 +129,6 @@ namespace Tests.Algorithm
                 StackPop(_list1, false);
                 B2 += blast;
 
-                if (Func(_list1, A, B2) && Func(_list, A, B1))
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-        }
-
-        private bool AGetEndSide(List<int> list, int A, int B)
-        {
-            var last = list.LastOrDefault();
-            A += last;
-            StackPop(list, false);
-
-            if (list.Count == 0)
-            {
-                if (A >= B)
-                {
-                    Debug.WriteLine($"当前 A =={A} B == {B}");
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else if (list.Count == 1)
-            {
-                B += list.FirstOrDefault();
-                StackPop(list, false);
-                if (A >= B)
-                {
-                    Debug.WriteLine($"当前 A =={A} B == {B}");
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                var bfirst = list.FirstOrDefault();
-                var blast = list.LastOrDefault();
-
-                // 分两种情况 取前值 取后值
-                var cache = new int[list.Count];
-                var cache1 = new int[list.Count];
-                list.CopyTo(cache);
-                list.CopyTo(cache1);
-                var B1 = B;
-                var B2 = B;
-                var _list = cache.ToList();
-                var _list1 = cache1.ToList();
-
-                // 取前值
-                StackPop(_list, true);
-                B1 += bfirst;
-
-                // 取后值
-                StackPop(_list1, false);
-                B2 += blast;
                 if (Func(_list1, A, B2) && Func(_list, A, B1))
                 {
                     return true;
@@ -195,13 +144,13 @@ namespace Tests.Algorithm
         {
             if (ishead)
             {
-                Debug.WriteLine("删除头部：" + list.FirstOrDefault());
+                //Debug.WriteLine("删除头部：" + list.FirstOrDefault());
                 list.RemoveAt(0);
 
             }
             else
             {
-                Debug.WriteLine("删除尾部：" + list.LastOrDefault());
+                //Debug.WriteLine("删除尾部：" + list.LastOrDefault());
                 list.RemoveAt(list.Count - 1);
             }
         }
